c397995678153e5f0208afcba050693b8a4e955a
Sparsh Gumasta <sparshgumasta899@gmail.com>
2025-09-07 16:16:36 +0530

    Update Crop.jsx

--- frontend/src/components/Crop.jsx
+++ frontend/src/components/Crop.jsx
@@ -131,7 +131,7 @@ export default function CropPage() {
                     <a
                       href={croppedUrl}
                       download="cropped.png"
-                      className="btn inline-block mt-2"
+                      className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800 mt-2"
                     >
                       Download
                     </a>
